f3a84e28d515514ea960e53945279b7145a9c800
David Ruffner <davidruffner924@gmail.com>
2021-09-02 17:35:27 -0400

    Initial Commit

---
+++ src/main/java/davidruffner/museio_backend/MuseioBackendApplication.java
@@ -0,0 +1,13 @@
+package davidruffner.museio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuseioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/davidruffner/museio_backend/MuseioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package davidruffner.museio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
